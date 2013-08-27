0571eee7908a506b486b7ed00bcfc40f95026800
Tom Hoen <thoen@edgevaleinteractive.com>
2013-08-27 17:30:24 -0400

    use the assetable polymorphism to set and retrieve what object the Redactor assets belongs to

--- app/controller/redactor_rails/pictures_controller.rb
+++ app/controller/redactor_rails/pictures_controller.rb
@@ -1,9 +1,9 @@
 class RedactorRails::PicturesController < ApplicationController
-  before_filter :redactor_authenticate_user! if RedactorRails.picture_model.new.respond_to?(RedactorRails.devise_user)
+  # before_filter :redactor_authenticate_user! if RedactorRails.picture_model.new.respond_to?(RedactorRails.devise_user)
 
   def index
     @pictures = RedactorRails.picture_model.where(
-        RedactorRails.picture_model.new.respond_to?(RedactorRails.devise_user) ? { RedactorRails.devise_user_key => redactor_current_user.id } : { })
+        { :assetable => redactor_asset_owner } )
     render :json => @pictures.to_json
   end
 
@@ -12,10 +12,8 @@ class RedactorRails::PicturesController < ApplicationController
 
     file = params[:file]
     @picture.data = RedactorRails::Http.normalize_param(file, request)
-    if @picture.respond_to?(RedactorRails.devise_user)
-      @picture.send("#{RedactorRails.devise_user}=", redactor_current_user)
-      @picture.assetable = redactor_current_user
-    end
+
+    @picture.assetable = redactor_asset_owner
 
     if @picture.save
       render :text => { :filelink => @picture.url }.to_json
